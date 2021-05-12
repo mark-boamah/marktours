061110927f4089300774060108272c8c5696f383
Boamah Mark (s112576) <mark.boamah@student.ap.be>
2021-05-12 08:48:09 +0200

    Updates

---
+++ precache-manifest.3d25b128d35da064b9ac31879d1c4a9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16d2f1ccfc9c0ea610cc",
+    "url": "/marktours/static/css/main.cf664d21.chunk.css"
+  },
+  {
+    "revision": "16d2f1ccfc9c0ea610cc",
+    "url": "/marktours/static/js/main.16d2f1cc.chunk.js"
+  },
+  {
+    "revision": "236f7dee556e4c439e3e",
+    "url": "/marktours/static/css/1.7006bc05.chunk.css"
+  },
+  {
+    "revision": "236f7dee556e4c439e3e",
+    "url": "/marktours/static/js/1.236f7dee.chunk.js"
+  },
+  {
+    "revision": "0566c438d88400ebacfa",
+    "url": "/marktours/static/js/runtime~main.0566c438.js"
+  },
+  {
+    "revision": "d9b0323b57263e9982295012aff40456",
+    "url": "/marktours/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-toeristen-mmt/precache-manifest.600dc2afa4156252087b8fd14aa85fdf.js"
+  "/marktours/precache-manifest.3d25b128d35da064b9ac31879d1c4a9e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-toeristen-mmt/index.html", {
+workbox.routing.registerNavigationRoute("/marktours/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
