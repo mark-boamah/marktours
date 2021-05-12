e92a2f318603cd44ca120d5c6fec05fa4c6b61ab
Boamah Mark (s112576) <mark.boamah@student.ap.be>
2021-05-12 08:39:20 +0200

    Updates

---
+++ precache-manifest.600dc2afa4156252087b8fd14aa85fdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16d2f1ccfc9c0ea610cc",
+    "url": "/react-toeristen-mmt/static/css/main.cf664d21.chunk.css"
+  },
+  {
+    "revision": "16d2f1ccfc9c0ea610cc",
+    "url": "/react-toeristen-mmt/static/js/main.16d2f1cc.chunk.js"
+  },
+  {
+    "revision": "18ec97c558c349330d01",
+    "url": "/react-toeristen-mmt/static/css/1.7006bc05.chunk.css"
+  },
+  {
+    "revision": "18ec97c558c349330d01",
+    "url": "/react-toeristen-mmt/static/js/1.18ec97c5.chunk.js"
+  },
+  {
+    "revision": "7493d37a6d88303908a8",
+    "url": "/react-toeristen-mmt/static/js/runtime~main.7493d37a.js"
+  },
+  {
+    "revision": "9fa2b0c5cb7b33170d0a77ee5f330290",
+    "url": "/react-toeristen-mmt/index.html"
+  }
+];
